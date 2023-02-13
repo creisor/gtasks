7f38f801c4a8d13bba4cc7209ea25676f76f9782
creisor <chris.reisor@provi.com>
2023-02-13 12:06:42 -0600

    cleans up git ignore and starts making libs to split up work

--- gtasks/app.py
+++ gtasks/app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+# for CLI
 import click
 from .commands import view
 
 @click.group(help="A commandline interface to Google Tasks")
-def main():
+def cli():
     pass
+cli.add_command(view.view)
 
-main.add_command(view.view)
+def main():
+    cli()
+
 
 if __name__ == '__main__':
     main()
--- gtasks/commands/view.py
+++ gtasks/commands/view.py
@@ -1,9 +1,18 @@
 import click
 
+from ..lib import auth
+
 @click.command()
-@click.option('--yesterday', is_flag=True, help='See yesterday\'s tasks')
-def view(yesterday):
-    if yesterday:
+@click.option('-y', '--yesterday', is_flag=True, help='See yesterday\'s tasks')
+@click.option('-a', '--all-tasks', is_flag=True, help='See tasks from all tasklists')
+def view(yesterday, all_tasks):
+    """Allows you to view tasks in your list(s)"""
+
+    auth.authenticate()
+
+    if all_tasks:
+        print(f'view all tasks')
+    elif yesterday:
         print(f'view yesterday')
     else:
         print(f'view today')
